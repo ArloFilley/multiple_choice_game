b54272db0b827ac3a34b27958a61ff4e7f08a5be
ArloFilley <ArloFilley@gmail.com>
2022-10-16 19:46:38 +0100

    intial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::{io,fs};
+
+pub mod terminal;
+fn main() {
+    loop {
+        terminal::clear_screen();
+        terminal::cursor_position(0, 0);
+        let mut choices = String::from("");
+        read_file();
+        for line in io::stdin().lines() {
+            let input = line.unwrap();
+            choices = read_choice(&choices, &input);
+            if choices.eq("end") {
+                break;
+            }
+        }
+        if choices.eq("end") {
+            break;
+        }
+    }
+}
+
+fn read_file() {
+    let intro = fs::read_to_string("story.txt").unwrap();
+    for (i, line) in intro.lines().enumerate() {
+        if i == 0 {
+            let _pointer = line;
+        } else {
+            println!("{}",line);
+        }
+    }
+}
+
+fn read_choice(choices: &str, input: &str) -> String {
+    let mut choices = String::from(choices);
+    if input.len() > 0 {
+        choices.push_str(&input[0..1]);
+        if input[0..1].eq("q") {
+            choices = String::from("end");
+        }
+    }
+    choices
+}
---
+++ src/terminal.rs
@@ -0,0 +1,10 @@
+pub fn cursor_position(x: usize, y: usize) {
+    let x = x.saturating_add(1);
+    let y = y.saturating_add(1);
+    print!("{}", termion::cursor::Goto(x as u16, y as u16));
+}
+
+
+pub fn clear_screen() {
+    print!("{}", termion::clear::All)
+}
